9eef6bd385ef4394015fd2ff1138424e04807b1d
Emanru <emil.anbacken.rudfeldt@gmail.com>
2024-11-26 21:12:15 +0100

    alm0std0newithlabIV-I

--- src/components/OneBurger.vue
+++ src/components/OneBurger.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    {{ burger.name }} {{ burger.kCal }}
+  <div class="burger">
+    <h1>{{ burger.name }}</h1>
+    <img v-bind:src="burger.img">
+    <p>{{ burger.kCal }} kCal</p>
+    <p v-if="burger.gluten == true || burger.lactose == true">Allergens: </p>
+    <ul class="allergens">
+      <li v-if="burger.gluten == true">Gluten</li>
+      <li v-if="burger.lactose == true">Lactose</li>
+    </ul>
+    <button v-on:click="addBurger">Add</button>
+    <button v-on:click="removeBurger">Remove</button>
+    <p>{{ amountOrdered }}</p>
   </div>
 </template>
 
@@ -9,11 +19,35 @@ export default {
   name: 'OneBurger',
   props: {
     burger: Object
+  },
+  data: function () {
+    return {
+      amountOrdered: 0,
+    }
+  },
+  methods: {
+    addBurger: function() {
+      this.amountOrdered++;
+    },
+    removeBurger: function() {
+      this.amountOrdered--;
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1 {
+  font-size: 16pt;
+}
 
+.burger {
+    margin: 5px;
+    padding: 5px;
+}
+
+.burger img {
+    width:100%;
+    height:auto;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,142 +1,100 @@
 <template>
-<div>
-    <div>
-    <h1>Burgers</h1>
-    <Burger v-for="burger in burgers"
-            v-bind:burger="burger" 
-            v-bind:key="burger.name"/>
-    </div>
-    <div id="map" v-on:click="addOrder">
-    click here
-    </div>
-    <input type="text" v-model="yourVariable">
-    <div>
-    {{ yourVariable }}
-    </div>
-</div>
-<header>
-            <h1>Welcome to All Things Burger!</h1>
-        </header>
-        <main>
-            <section class = "burgersection">
-                <h2>Select burger</h2>
-                <p>This is where you execute burger selection</p>
-                <div class="burger-wrapper">
-                    <div class="burger">
-                        <h2>The Fire Burger</h2>
-                        <P>It doesn't get more volcano than this</P>
-                        <img src="/img/fireburger.jpg" alt="Span">
-                        <ul>
-                            <li>764 kCal</li>
-                            <li>Contains<span class="ingredients">gluten</span></li>
-                            <li>Contains<span class="ingredients">lactose</span></li>
-                        </ul>
-                    </div>
-                    <div class="burger">
-                        <h2>The Burger Stack</h2>
-                        <p>Overflow ahoy!</p>
-                        <img src="/img/stackofburgers.jpg" alt="Span">
-                        <ul>
-                            <li>2292 kCal</li>
-                            <li>Contains<span class="ingredients">gluten</span></li>
-                            <li>Contains<span class="ingredients">lactose</span></li>
-                        </ul>
-                    </div>
-                    <div class="burger">
-                        <h2>The Horse Burger</h2>
-                        <p>Did someone say "neigh"?</p>
-                        <img src="/img/horseburger.jpg" alt="Span">
-                        <ul>
-                            <li>468 kCal</li>
-                            <li>Contains<span class="ingredients">gluten</span></li>
-                            <li>Contains<span class="ingredients">horse</span></li>
-                        </ul>
-                    </div>
-                </div>
-            </section>
-            <section class="contactsection">
-                <form>
-                    <h2>Customer Information</h2>
-                    <p>This is where you provide necessary information</p>
-                    <h2>Delivery Information</h2>
-                    <p>
-                        <label for="fullname">Full name</label><br>
-                        <input type="text" id="fullname" name="fn" required="required" placeholder="First- and Last name">
-                    </p>
-                    <p>
-                        <label for="email">E-mail</label><br>
-                        <input type="email" id="email" name="em" required="required" placeholder="E-mail adress">
-                    </p>
-                    <p>
-                        <label for="street">Street</label><br>
-                        <input type="text" id="street" name="st" required="required" placeholder="Street name">
-                    </p>
-                    <p>
-                        <label for="house">House</label><br>
-                        <input type="number" id="house" name="fn" required="required" placeholder="House number">
-                    </p>
-                    <p>
-                        <label for="payment">Payment options</label><br>
-                        <select id="payment" name="pay">
-                            <option selected="selected">Credit card</option>
-                            <option>Swish</option>
-                            <option>Klarna</option>
-                            <option>PayPal</option>
-                            <option>Cash</option>
-                        </select>
-                    </p>
-                    <p>Gender</p>
-                    <p>
-                        <input type="radio" id="male" name="gender" value="male">
-                        <label for="male">Male</label>
-                    </p>
-                    <p>
-                        <input type="radio" id="female" name="gender" value="female">
-                        <label for="female">Female</label>
-                    </p>
-                    <p>
-                        <input type="radio" id="other" name="gender" value="other">
-                        <label for="other">Other</label>
-                    </p>
-                    <p>
-                        <input type="radio" id="undisclosed" name="gender" value="undisclosed" checked>
-                        <label for="undisclosed">Undisclosed</label>
-                    </p>
-
-                </form>
-
-            </section>
-        </main>
-        <button type="submit">
-            <img src="/img/delivery.png">
-            Place my order!
-        </button>
-        <hr>
-        <footer>
-            &copy; Emanru Creative Group Inc.
-        </footer>
+  <header>
+    <h1>Welcome to All Things Burger!</h1>
+  </header>
+  <main>
+    <section class = "burgersection">
+      <h2>Select burger</h2>
+      <p>This is where you execute burger selection</p>
+      <div class="burger-wrapper">
+        <Burger v-for="burger in burgers"
+                v-bind:burger="burger"
+                v-bind:key="burger.name"/>
+      </div>
+    </section>
+    <section class="contactsection">
+      <form>
+        <h2>Customer Information</h2>
+        <p>This is where you provide necessary information</p>
+        <h2>Delivery Information</h2>
+        <p>
+          <label for="fullname">Full name</label><br>
+          <input type="text" v-model="fullname" id="fullname" required="required" placeholder="First- and Last name">
+        </p>
+        <p>
+          <label for="email">E-mail</label><br>
+          <input type="email" v-model="email" id="email" required="required" placeholder="E-mail adress">
+        </p>
+        <p>
+          <label for="street">Street</label><br>
+          <input type="text" v-model="street" id="street" required="required" placeholder="Street name">
+        </p>
+        <p>
+          <label for="house">House</label><br>
+          <input type="number" v-model="house" id="house" name="fn" required="required" placeholder="House number">
+        </p>
+        <p>
+          <label for="payment">Payment options</label><br>
+          <select id="payment" name="pay">
+            <option selected="selected">Credit card</option>
+            <option>Swish</option>
+            <option>Klarna</option>
+            <option>PayPal</option>
+            <option>Cash</option>
+          </select>
+        </p>
+        <p>Gender</p>
+        <p>
+          <input type="radio" v-model="gender" id="male" value="male">
+          <label for="male">Male</label>
+        </p>
+        <p>
+          <input type="radio" v-model="gender" id="female" value="female">
+          <label for="female">Female</label>
+        </p>
+        <p>
+          <input type="radio" v-model="gender" id="other" value="other">
+          <label for="other">Other</label>
+        </p>
+        <p>
+          <input type="radio" v-model="gender" id="undisclosed" value="undisclosed" checked>
+          <label for="undisclosed">Undisclosed</label>
+        </p>
+      </form>
+    </section>
+  </main>
+  <button v-on:click="placeOrder()" type="submit">
+    <img src="/img/delivery.png">
+    Place my order!
+  </button>
+  <hr>
+  <footer>
+    &copy; Emanru Creative Group Inc.
+  </footer>
 </template>
 
 <script>
 import Burger from '../components/OneBurger.vue'
 import io from 'socket.io-client'
+import menu from '../assets/menu.json'
 
 const socket = io("localhost:3000");
 
 function MenuItem(name, url, kCal, glu, lac) {
   this.name = name;
-  this.URL = url;
+  this.img = url;
   this.kCal = kCal;
   this.gluten = glu;
   this.lactose = lac;
 }
 
-const burgerArray = [new MenuItem("Fireburger", "/img/fireburger.jpg", 764, true, true),
+const burgerArray = [menu[0], menu[1], menu[2]]
+
+/* const burgerArray = [new MenuItem("Fireburger", "/img/fireburger.jpg", 764, true, true),
                  new MenuItem("Burgerstack", "/img/stackofburgers.jpg", 2292, true, true),
                  new MenuItem("Horseburger", "/img/horseburger.jpg", 468, true, false)
                 ]
-
-console.log(burgerArray);
+*/
 
 export default {
   name: 'HomeView',
@@ -146,7 +104,11 @@ export default {
   data: function () {
     return {
       burgers: burgerArray,
-      yourVariable: "Välj en burgare"         
+      fullname: '',
+      email: '',
+      street: '',
+      house: '',
+      gender: 'undisclosed'
     }
   },
   methods: {
@@ -162,6 +124,9 @@ export default {
                                 orderItems: ["Beans", "Curry"]
                               }
                  );
+    },
+    placeOrder: function () {
+      console.log(this.fullname, this.email, this.street, this.house, this.gender);
     }
   }
 }
@@ -176,93 +141,94 @@ export default {
 
   @import url('https://fonts.googleapis.com/css2?family=Agbalumo&family=Cormorant:wght@700&display=swap');
 
-header {
-    background-image: url("/img/headerimg.png");
-    background-size: cover;
-    overflow: hidden;
-    width: 100%;
-    height: 200px;
-    margin: 10px;
-    opacity: 0.5;
-}
+  header {
+      background-image: url("/img/headerimg.png");
+      background-size: cover;
+      overflow: hidden;
+      width: 100%;
+      height: 200px;
+      margin: 10px;
+      opacity: 0.5;
+  }
 
-header h1 {
-    width: 40rem;
-    margin: 50px auto;
-    text-align: center;
-    color: black;
-}
+  header h1 {
+      width: 40rem;
+      margin: 50px auto;
+      text-align: center;
+      color: black;
+  }
 
 
-body {
-    font-family: 'Times New Roman', Times, serif;
-    font-size: 16pt;
-}
+  body {
+      font-family: 'Times New Roman', Times, serif;
+      font-size: 16pt;
+  }
 
-h2 {
-    font-family:'Times New Roman', Times, serif;
-    font-size: 20pt;
-}
+  h2 {
+      font-family:'Times New Roman', Times, serif;
+      font-size: 20pt;
+  }
 
-.burgersection {
-    background-color: black;
-    color:white;
-    border: 4px dashed white;
-    padding: 5px;
-    margin: 10px;
-}
+  .burgersection {
+      background-color: black;
+      color:white;
+      border: 4px dashed white;
+      padding: 5px;
+      margin: 10px;
+  }
 
-.burger-wrapper {
-    display: grid;
-    grid-gap: 10px;
-    grid-template-columns: 25% 25% 25%;
-}
+  .burger-wrapper {
+      display: grid;
+      grid-gap: 10px;
+      grid-template-columns: 25% 25% 25%;
+      margin: 10px;
+  }
 
-.burger {
-    margin: 5px;
-    padding: 5px;
-}
+  .burger {
+      margin: 5px;
+      padding: 5px;
+  }
 
-.burger img {
-    width:100%;
-    height:auto;
-}
+  .burger img {
+      width:100%;
+      height:auto;
+  }
 
-.ingredients {
-    font-weight: bold;
-    margin: auto 5px;
-}
+  .ingredients {
+      font-weight: bold;
+      margin: auto 5px;
+  }
 
-.contactsection {
-    background-color: white;
-    margin: 10px;
-    padding: 10px;
-    border: 4px dashed black;
-}
+  .contactsection {
+      background-color: white;
+      margin: 10px;
+      padding: 10px;
+      border: 4px dashed black;
+  }
 
-button {
-    margin: 10px;
-}
+  button {
+      margin: 10px;
+  }
 
-button img {
-    height: 30px;
-    width: 30px;
-    margin-top: 5px;
-    margin-bottom: 5px;
-    margin-left: 0px;
-}
+  button img {
+      height: 30px;
+      width: 30px;
+      margin-top: 5px;
+      margin-bottom: 5px;
+      margin-left: 0px;
+  }
 
-button:hover {
-    background-color: rgb(93, 152, 93);
-    cursor: pointer;
-}
+  button:hover {
+      background-color: rgb(93, 152, 93);
+      cursor: pointer;
+  }
 
-button:active {
-    background-color: rgb(48, 89, 60);
-}
+  button:active {
+      background-color: rgb(48, 89, 60);
+  }
 
-footer {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 10pt;
-}
+  footer {
+      font-family: Arial, Helvetica, sans-serif;
+      font-size: 10pt;
+  }
 </style>
